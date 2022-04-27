76379e997a86d35b7ed5537194bcd7eb81b75687
59114_Chetana <deslechetna1999@gmail.com>
2022-04-27 22:15:11 +0530

    [Chetana]Added: Maximum in given Integer

---
+++ GenericsProgram/PrintArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    public class PrintArray
+    {
+        public void MaxElement(int[] arr)
+        {
+            int i;
+            int max = arr[0];
+            for (i = 1; i < arr.Length; i++)
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            Console.WriteLine("Maximum in given " + "array is " + max);
+
+
+        }
+    }
+}
--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GenericsProgram;
+using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        int[] arr = { 10, 324, 45, 90, 980 };
+        PrintArray array = new PrintArray();
+        array.MaxElement(arr);
+
+    }
+}
